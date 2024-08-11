dcc9b925905387a1a3db35f1ba46aa735993b653
ysyx_23060204-Zhang <1755613781@qq.com>
2024-08-12 06:14:17 +0800

    update chisel 113MHz

--- core/core/src/uart_ctrl.scala
+++ core/core/src/uart_ctrl.scala
@@ -3,7 +3,7 @@ import chisel3.util._
 import bundles._
 
 class async_transmitter extends BlackBox(Map(
-    "ClkFrequency" -> 110000000,
+    "ClkFrequency" -> 113000000,
     "Baud" -> 9600
 )) {
     val io = IO(new Bundle {
@@ -16,7 +16,7 @@ class async_transmitter extends BlackBox(Map(
 }
 
 class async_receiver extends BlackBox(Map(
-    "ClkFrequency" -> 110000000,
+    "ClkFrequency" -> 113000000,
     "Baud" -> 9600
 )) {
     val io = IO(new Bundle {
